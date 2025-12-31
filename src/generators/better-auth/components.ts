296c93cc060408264cbe48fdebe091aff4d82cc2
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-12-31 11:52:19 +0530

    changes for the tsconfig for the tsx file

--- src/generators/better-auth/components.ts
+++ src/generators/better-auth/components.ts
@@ -1,15 +1,12 @@
-import { writeFileWithDir } from "../../file-writer";
+import { writeFileWithDir } from '../../file-writer'
 import {
   getSignInTemplate,
   getSignUpTemplate,
   getUserButtonTemplate,
-} from "../templates/better-auth/components/sign-in.template";
+} from '../templates/better-auth/components/sign-in.template'
 
 export async function generateComponents(componentsDir: string): Promise<void> {
-  await writeFileWithDir(`${componentsDir}/sign-in.tsx`, getSignInTemplate());
-  await writeFileWithDir(`${componentsDir}/sign-up.tsx`, getSignUpTemplate());
-  await writeFileWithDir(
-    `${componentsDir}/user-button.tsx`,
-    getUserButtonTemplate()
-  );
+  await writeFileWithDir(`${componentsDir}/sign-in.tsx`, getSignInTemplate())
+  await writeFileWithDir(`${componentsDir}/sign-up.tsx`, getSignUpTemplate())
+  await writeFileWithDir(`${componentsDir}/user-button.tsx`, getUserButtonTemplate())
 }
